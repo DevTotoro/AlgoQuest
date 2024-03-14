2b100e60f19113e7f42c619cf200fb34132f1a9b
DevTotoro <totoro.softwaredev@gmail.com>
2024-03-14 23:20:40 +0200

    Add Player rotation

--- Assets/Scripts/Actors/Player/PlayerController.cs
+++ Assets/Scripts/Actors/Player/PlayerController.cs
@@ -9,12 +9,14 @@ namespace Actors.Player
     {
         [Header("Movement")]
         [SerializeField] [Range(0f, 50f)] private float movementSpeed = 5f;
+        [SerializeField] [Range(0f, 50f)] private float rotationSpeed = 5f;
 
         [Header("References")]
         [SerializeField] private CinemachineVirtualCamera virtualCamera;
         [SerializeField] private AudioListener audioListener;
         
         private Vector3 _movementDirection = Vector3.zero;
+        private Vector3 _rotationDirection = Vector3.zero;
         private float _movementDistance;
 
         private void OnEnable()
@@ -32,6 +34,7 @@ namespace Actors.Player
             _movementDistance = movementSpeed * Time.deltaTime;
             
             HandleMovement();
+            HandleRotation();
         }
         
         public override void OnNetworkSpawn()
@@ -55,6 +58,7 @@ namespace Actors.Player
             if (!IsOwner) return;
             
             _movementDirection = new Vector3(input.x, 0f, input.y);
+            _rotationDirection = _movementDirection;
         }
         
         // ====================
@@ -64,6 +68,14 @@ namespace Actors.Player
             if (Core.GameManager.Singleton.IsPaused || _movementDirection == Vector3.zero) return;
             
             transform.Translate(_movementDirection * _movementDistance, Space.World);
+        }
+
+        private void HandleRotation()
+        {
+            if (Core.GameManager.Singleton.IsPaused || _rotationDirection == Vector3.zero) return;
+            
+            var targetRotation = Quaternion.LookRotation(_rotationDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
     }
 }
